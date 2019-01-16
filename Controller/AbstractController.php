fa0fa899b307c039666f2cd291bb58d0217c2e0f
webeweb <support@webeweb.fr>
2019-01-16 18:57:12 +0100

    Add abstract controller skeleton

---
+++ Controller/AbstractController.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the smsmode-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SMSModeBundle\Controller;
+
+use WBW\Bundle\CoreBundle\Controller\AbstractController as BaseController;
+
+/**
+ * Abstract controller
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SMSModeBundle\Controller
+ * @abstract
+ */
+abstract class AbstractController extends BaseController {
+
+}
