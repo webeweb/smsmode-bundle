3dfd624afba002e81fdd23ae76924768b2e9e2f1
webeweb <support@webeweb.fr>
2019-01-16 18:55:52 +0100

    Add Test kernel

---
+++ Tests/Fixtures/app/TestKernel.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use WBW\Bundle\CoreBundle\Tests\AbstractKernel;
+
+/**
+ * Test kernel.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SMSModeBundle\Tests\Fixtures
+ */
+class TestKernel extends AbstractKernel {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles() {
+        $bundles = [
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new WBW\Bundle\CoreBundle\CoreBundle(),
+            new WBW\Bundle\SMSModeBundle\SMSModeBundle(),
+        ];
+        return $bundles;
+    }
+}
