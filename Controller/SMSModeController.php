ae9577d54bf717bc57ac1494e9f604dfc0e0a699
webeweb <support@webeweb.fr>
2019-02-04 19:03:44 +0100

    Add sMsmode controller

---
+++ Controller/SMSModeController.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * This file is part of the smsmode-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SMSModeBundle\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use WBW\Bundle\SMSModeBundle\Event\DeliveryReportCallbackEvent;
+use WBW\Bundle\SMSModeBundle\Event\SMSModeEvents;
+use WBW\Bundle\SMSModeBundle\Event\SMSReplyCallbackEvent;
+
+/**
+ * sMsmode controller
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SMSModeBundle\Controller
+ */
+class SMSModeController extends AbstractController {
+
+    /**
+     * Delivery report callback.
+     *
+     * @param Request $request The request.
+     * @return Response Returns the response.
+     */
+    public function deliveryReportCallbackAction(Request $request) {
+
+        $eventName = SMSModeEvents::DELIVERY_REPORT_CALLBACK;
+
+        $eventDispatcher = $this->getEventDispatcher();
+        if (null !== $eventDispatcher && true === $eventDispatcher->hasListeners($eventName)) {
+
+            $this->getLogger()->info(sprintf("sMsmode controller dispatch an event with name \"%s\"", $eventName));
+
+            $deliveryReportCallback = $this->newDeliveryReportCallback($request);
+
+            $event = new DeliveryReportCallbackEvent($eventName, $deliveryReportCallback);
+
+            $eventDispatcher->dispatch($eventName, $event);
+        }
+
+        return new JsonResponse(["code" => 200, "message" => "OK"]);
+    }
+
+    /**
+     * SMS reply callback.
+     *
+     * @param Request $request The request.
+     * @return Response Returns the response.
+     */
+    public function smsReplyCallbackAction(Request $request) {
+
+        $eventName = SMSModeEvents::SMS_REPLY_CALLBACK;
+
+        $eventDispatcher = $this->getEventDispatcher();
+        if (null !== $eventDispatcher && true === $eventDispatcher->hasListeners($eventName)) {
+
+            $this->getLogger()->info(sprintf("sMsmode controller dispatch an event with name \"%s\"", $eventName));
+
+            $smsReplyCallback = $this->newSMSReplyCallback($request);
+
+            $event = new SMSReplyCallbackEvent($eventName, $smsReplyCallback);
+
+            $eventDispatcher->dispatch($eventName, $event);
+        }
+
+        return new JsonResponse(["code" => 200, "message" => "OK"]);
+    }
+}
---
+++ Tests/Controller/SMSModeControllerTest.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the smsmode-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SMSModeBundle\Tests\Controller;
+
+use WBW\Bundle\SMSModeBundle\Tests\AbstractWebTestCase;
+
+/**
+ * sMsmode controller test
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SMSModeBundle\Tests\Controller
+ */
+class SMSModeControllerTest extends AbstractWebTestCase {
+
+    /**
+     * Tests the deliveryReportCallbackAction() method.
+     *
+     * @return void
+     */
+    public function testDeliveryReportCallbackAction() {
+
+        // Create a client.
+        $client = static::createClient();
+
+        // Make a GET request with XML HTTP request.
+        $client->request("GET", "/delivery-report-callback?numero=33601020304&date_reception=2010-03-25+09%3A52%3A17&statut=11&smsID=S7EpYZ5kmS87&refClient=12azer34&mcc_mnc=20801");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals("application/json", $client->getResponse()->headers->get("Content-Type"));
+
+        // Check the JSON response.
+        $res = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey("code", $res);
+        $this->assertArrayHasKey("message", $res);
+
+        $this->assertEquals(200, $res["code"]);
+        $this->assertEquals("OK", $res["message"]);
+    }
+
+    /**
+     * Tests the smsReplyCallbackAction() method.
+     *
+     * @return void
+     */
+    public function testSmsReplyCallbackAction() {
+
+        // Create a client.
+        $client = static::createClient();
+
+        // Make a GET request with XML HTTP request.
+        $client->request("GET", "/sms-reply-callback?numero=36034&message=bonjour&emetteur=33601020304&date_reception=01012013-122233&smsID=abcd1234&refClient=monclient123&responseID=azertyu123");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals("application/json", $client->getResponse()->headers->get("Content-Type"));
+
+        // Check the JSON response.
+        $res = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey("code", $res);
+        $this->assertArrayHasKey("message", $res);
+
+        $this->assertEquals(200, $res["code"]);
+        $this->assertEquals("OK", $res["message"]);
+    }
+}
