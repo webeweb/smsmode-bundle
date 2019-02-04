dec468c498248c3f9a8bcfacb9830cf72e84034c
webeweb <support@webeweb.fr>
2019-02-04 19:04:57 +0100

    Add Test event listener

---
+++ Tests/Fixtures/EventListener/TestEventListener.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the smsmode-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SMSModeBundle\Tests\Fixtures\EventListener;
+
+use Symfony\Component\EventDispatcher\Event;
+use WBW\Bundle\SMSModeBundle\Event\DeliveryReportCallbackEvent;
+use WBW\Bundle\SMSModeBundle\Event\SMSReplyCallbackEvent;
+
+/**
+ * Test event listener.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SMSModeBundle\Tests\Fixtures\EventListener
+ */
+class TestEventListener {
+
+    /**
+     * On delivery report callback.
+     *
+     * @param DeliveryReportCallbackEvent $event The event.
+     * @return Event Returns the event.
+     */
+    public function onDeliveryReportCallback(DeliveryReportCallbackEvent $event) {
+        return $event;
+    }
+
+    /**
+     * On SMS reply callback.
+     *
+     * @param SMSReplyCallbackEvent $event The event.
+     * @return Event Returns the event.
+     */
+    public function onSMSReplyCallback(SMSReplyCallbackEvent $event) {
+        return $event;
+    }
+}
