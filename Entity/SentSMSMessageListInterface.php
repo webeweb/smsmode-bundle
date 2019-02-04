b1a2579191bc58a4e9a4812c96fdaf0776a4840a
webeweb <support@webeweb.fr>
2019-02-04 20:56:37 +0100

    Add Sent SMS message list interface

---
+++ Entity/SentSMSMessageListInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the smsmode-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SMSModeBundle\Entity;
+
+/**
+ * Sent SMS message list interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SMSModeBundle\Entity
+ */
+interface SentSMSMessageListInterface {
+
+    /**
+     * Get the offset.
+     *
+     * @return int Returns the offset.
+     */
+    public function getSMSModeOffset();
+}
