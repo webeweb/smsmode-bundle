f5d2ee6821a532ae576312477fbb2b4deb4f2171
webeweb <support@webeweb.fr>
2019-02-04 20:55:08 +0100

    Add Delivery report interface

---
+++ Entity/DeliveryReportInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the smsmode-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SMSModeBundle\Entity;
+
+/**
+ * Delivery report interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SMSModeBundle\Entity
+ */
+interface DeliveryReportInterface {
+
+    /**
+     * Get the SMS ID.
+     *
+     * @return string Returns the SMS ID.
+     */
+    public function getSMSModeSmsID();
+}
