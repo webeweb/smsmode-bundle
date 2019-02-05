511c330faadc6f27e96e124b50570a40cbcdc3db
webeweb <support@webeweb.fr>
2019-02-05 18:04:45 +0100

    Add Transferring credits event

---
+++ Event/TransferringCreditsEvent.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of the smsmode-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SMSModeBundle\Event;
+
+use WBW\Bundle\SMSModeBundle\Entity\TransferringCreditsInterface;
+use WBW\Library\SMSMode\Model\TransferringCreditsRequest;
+use WBW\Library\SMSMode\Model\TransferringCreditsResponse;
+
+/**
+ * Transferring credits event
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SMSModeBundle\Event
+ */
+class TransferringCreditsEvent extends AbstractResponseEvent {
+
+    /**
+     * Transferring credits.
+     *
+     * @var TransferringCreditsInterface
+     */
+    private $transferringCredits;
+
+    /**
+     * Constructor.
+     *
+     * @param TransferringCreditsInterface $transferringCredits The transferring credits.
+     */
+    public function __construct(TransferringCreditsInterface $transferringCredits) {
+        parent::__construct(SMSModeEvents::TRANSFERRING_CREDITS);
+        $this->setTransferringCredits($transferringCredits);
+    }
+
+    /**
+     * Get the transferring credits request.
+     *
+     * @return TransferringCreditsRequest Returns the transferring credits request.
+     */
+    public function getRequest() {
+        return parent::getRequest();
+    }
+
+    /**
+     * Get the transferring credits response.
+     *
+     * @return TransferringCreditsResponse Returns the transferring credits response.
+     */
+    public function getResponse() {
+        return parent::getResponse();
+    }
+
+    /**
+     * Get the transferring credits.
+     *
+     * @return TransferringCreditsInterface Returns the transferring credits.
+     */
+    public function getTransferringCredits() {
+        return $this->transferringCredits;
+    }
+
+    /**
+     * Set the transferring credits.
+     *
+     * @param TransferringCreditsInterface $transferringCredits The transferring credits.
+     * @return TransferringCreditsEvent Returns this transferring credits event.
+     */
+    protected function setTransferringCredits(TransferringCreditsInterface $transferringCredits) {
+        $this->transferringCredits = $transferringCredits;
+        return $this;
+    }
+}
---
+++ Tests/Event/TransferringCreditsEventTest.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * This file is part of the smsmode-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SMSModeBundle\Tests\Event;
+
+use WBW\Bundle\SMSModeBundle\Event\SMSModeEvents;
+use WBW\Bundle\SMSModeBundle\Event\TransferringCreditsEvent;
+use WBW\Bundle\SMSModeBundle\Tests\AbstractTestCase;
+use WBW\Library\SMSMode\Model\TransferringCreditsRequest;
+use WBW\Library\SMSMode\Model\TransferringCreditsResponse;
+
+/**
+ * Transferring credits event test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SMSModeBundle\Tests\Event
+ */
+class TransferringCreditsEventTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TransferringCreditsEvent($this->transferringCredits);
+
+        $this->assertEquals(SMSModeEvents::TRANSFERRING_CREDITS, $obj->getEventName());
+
+        $this->assertNull($obj->getRequest());
+        $this->assertNull($obj->getResponse());
+
+        $this->assertSame($this->transferringCredits, $obj->getTransferringCredits());
+    }
+
+    /**
+     * Tests the setRequest() method.
+     *
+     * @return void
+     */
+    public function testSetRequest() {
+
+        // Set a Transferring credits request mock.
+        $request = new TransferringCreditsRequest();
+
+        $obj = new TransferringCreditsEvent($this->transferringCredits);
+
+        $obj->setRequest($request);
+        $this->assertSame($request, $obj->getRequest());
+    }
+
+    /**
+     * Tests the setResponse() method.
+     *
+     * @return void
+     */
+    public function testSetResponse() {
+
+        // Set a Transferring credits response mock.
+        $response = new TransferringCreditsResponse();
+
+        $obj = new TransferringCreditsEvent($this->transferringCredits);
+
+        $obj->setResponse($response);
+        $this->assertSame($response, $obj->getResponse());
+    }
+}
