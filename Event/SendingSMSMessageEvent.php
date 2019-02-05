66809b20e0d1616dee018aa9cd029d87b4af39ab
webeweb <support@webeweb.fr>
2019-02-05 18:03:32 +0100

    Add Sending SMS message event

---
+++ Event/SendingSMSMessageEvent.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of the smsmode-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SMSModeBundle\Event;
+
+use WBW\Bundle\SMSModeBundle\Entity\SendingSMSMessageInterface;
+use WBW\Library\SMSMode\Model\SendingSMSMessageRequest;
+use WBW\Library\SMSMode\Model\SendingSMSMessageResponse;
+
+/**
+ * Sending SMS message event
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SMSModeBundle\Event
+ */
+class SendingSMSMessageEvent extends AbstractResponseEvent {
+
+    /**
+     * Sending SMS message.
+     *
+     * @var SendingSMSMessageInterface
+     */
+    private $sendingSMSMessage;
+
+    /**
+     * Constructor.
+     *
+     * @param SendingSMSMessageInterface $sendingSMSMessage The sending SMS message.
+     */
+    public function __construct(SendingSMSMessageInterface $sendingSMSMessage) {
+        parent::__construct(SMSModeEvents::SENDING_SMS_MESSAGE);
+        $this->setSendingSMSMessage($sendingSMSMessage);
+    }
+
+    /**
+     * Get the sending SMS message request.
+     *
+     * @return SendingSMSMessageRequest Returns the sending SMS message request.
+     */
+    public function getRequest() {
+        return parent::getRequest();
+    }
+
+    /**
+     * Get the sending SMS message response.
+     *
+     * @return SendingSMSMessageResponse Returns the sending SMS message response.
+     */
+    public function getResponse() {
+        return parent::getResponse();
+    }
+
+    /**
+     * Get the sending SMS message.
+     *
+     * @return SendingSMSMessageInterface Returns the sending SMS message.
+     */
+    public function getSendingSMSMessage() {
+        return $this->sendingSMSMessage;
+    }
+
+    /**
+     * Set the sending SMS message.
+     *
+     * @param SendingSMSMessageInterface $sendingSMSMessage The sending SMS message.
+     * @return SendingSMSMessageEvent Returns this sending SMS message event.
+     */
+    protected function setSendingSMSMessage(SendingSMSMessageInterface $sendingSMSMessage) {
+        $this->sendingSMSMessage = $sendingSMSMessage;
+        return $this;
+    }
+}
---
+++ Tests/Event/SendingSMSMessageEventTest.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * This file is part of the smsmode-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SMSModeBundle\Tests\Event;
+
+use WBW\Bundle\SMSModeBundle\Event\SendingSMSMessageEvent;
+use WBW\Bundle\SMSModeBundle\Event\SMSModeEvents;
+use WBW\Bundle\SMSModeBundle\Tests\AbstractTestCase;
+use WBW\Library\SMSMode\Model\SendingSMSMessageRequest;
+use WBW\Library\SMSMode\Model\SendingSMSMessageResponse;
+
+/**
+ * Sending SMS message event test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SMSModeBundle\Tests\Event
+ */
+class SendingSMSMessageEventTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new SendingSMSMessageEvent($this->sendingSMSMessage);
+
+        $this->assertEquals(SMSModeEvents::SENDING_SMS_MESSAGE, $obj->getEventName());
+
+        $this->assertNull($obj->getRequest());
+        $this->assertNull($obj->getResponse());
+
+        $this->assertSame($this->sendingSMSMessage, $obj->getSendingSMSMessage());
+    }
+
+    /**
+     * Tests the setRequest() method.
+     *
+     * @return void
+     */
+    public function testSetRequest() {
+
+        // Set a Sending SMS message request mock.
+        $request = new SendingSMSMessageRequest();
+
+        $obj = new SendingSMSMessageEvent($this->sendingSMSMessage);
+
+        $obj->setRequest($request);
+        $this->assertSame($request, $obj->getRequest());
+    }
+
+    /**
+     * Tests the setResponse() method.
+     *
+     * @return void
+     */
+    public function testSetResponse() {
+
+        // Set a Sending SMS message response mock.
+        $response = new SendingSMSMessageResponse();
+
+        $obj = new SendingSMSMessageEvent($this->sendingSMSMessage);
+
+        $obj->setResponse($response);
+        $this->assertSame($response, $obj->getResponse());
+    }
+}
