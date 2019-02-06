6d0c4831c928fecb2d178cd380fabbe406dfdc47
webeweb <support@webeweb.fr>
2019-02-06 18:09:01 +0100

    Update Deleting sub-account interface inheritance

--- Entity/DeletingSubAccountInterface.php
+++ Entity/DeletingSubAccountInterface.php
@@ -17,7 +17,7 @@ namespace WBW\Bundle\SMSModeBundle\Entity;
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\SMSModeBundle\Entity
  */
-interface DeletingSubAccountInterface {
+interface DeletingSubAccountInterface extends SMSModeEntityInterface {
 
     /**
      * Get the pseudo to delete.
