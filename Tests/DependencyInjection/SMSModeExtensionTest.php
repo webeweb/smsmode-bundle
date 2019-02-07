b04d4d6d88f68ba0b8205a59f874da873ee4cd55
webeweb <support@webeweb.fr>
2019-02-07 18:48:25 +0100

    Set sMsmode event listener as service

--- Tests/DependencyInjection/SMSModeExtensionTest.php
+++ Tests/DependencyInjection/SMSModeExtensionTest.php
@@ -11,7 +11,9 @@
 
 namespace WBW\Bundle\SMSModeBundle\Tests\DependencyInjection;
 
+use Exception;
 use WBW\Bundle\SMSModeBundle\DependencyInjection\SMSModeExtension;
+use WBW\Bundle\SMSModeBundle\EventListener\SMSModeEventListener;
 use WBW\Bundle\SMSModeBundle\Tests\AbstractTestCase;
 
 /**
@@ -26,11 +28,15 @@ class SMSModeExtensionTest extends AbstractTestCase {
      * Tests the load() method.
      *
      * @return void
+     * @throws Exception Throws an exception if an error occurs.
      */
     public function testLoad() {
 
         $obj = new SMSModeExtension();
 
         $this->assertNull($obj->load([], $this->containerBuilder));
+
+        // Event listeners.
+        $this->assertInstanceOf(SMSModeEventListener::class, $this->containerBuilder->get(SMSModeEventListener::SERVICE_NAME));
     }
 }
