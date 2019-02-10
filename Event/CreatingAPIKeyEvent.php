a2113c67d4a2d2df9951376aec95a348567c6925
webeweb <support@webeweb.fr>
2019-02-10 14:39:55 +0100

    Add Creating API key

---
+++ Event/CreatingAPIKeyEvent.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the smsmode-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SMSModeBundle\Event;
+
+use WBW\Library\SMSMode\Model\Request\CreatingAPIKeyRequest;
+use WBW\Library\SMSMode\Model\Response\CreatingAPIKeyResponse;
+
+/**
+ * Creating API key event.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SMSModeBundle\Event
+ */
+class CreatingAPIKeyEvent extends AbstractSMSModeEvent {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct(SMSModeEvents::CREATING_API_KEY, null);
+    }
+
+    /**
+     * Get the creating API key request.
+     *
+     * @return CreatingAPIKeyRequest Returns the creating API key request.
+     */
+    public function getRequest() {
+        return parent::getRequest();
+    }
+
+    /**
+     * Get the creating API key response.
+     *
+     * @return CreatingAPIKeyResponse Returns the creating API key response.
+     */
+    public function getResponse() {
+        return parent::getResponse();
+    }
+}
---
+++ Tests/Event/CreatingAPIKeyEventTest.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the smsmode-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SMSModeBundle\Tests\Event;
+
+use WBW\Bundle\SMSModeBundle\Event\CreatingAPIKeyEvent;
+use WBW\Bundle\SMSModeBundle\Event\SMSModeEvents;
+use WBW\Bundle\SMSModeBundle\Tests\AbstractTestCase;
+use WBW\Library\SMSMode\Model\Request\CreatingAPIKeyRequest;
+use WBW\Library\SMSMode\Model\Response\CreatingAPIKeyResponse;
+
+/**
+ * Creating API key event test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SMSModeBundle\Tests\Event
+ */
+class CreatingAPIKeyEventTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new CreatingAPIKeyEvent();
+
+        $this->assertEquals(SMSModeEvents::CREATING_API_KEY, $obj->getEventName());
+
+        $this->assertNull($obj->getRequest());
+        $this->assertNull($obj->getResponse());
+    }
+
+    /**
+     * Tests the setRequest() method.
+     *
+     * @return void
+     */
+    public function testSetRequest() {
+
+        // Set an Creating API key request mock.
+        $request = new CreatingAPIKeyRequest();
+
+        $obj = new CreatingAPIKeyEvent();
+
+        $obj->setRequest($request);
+        $this->assertSame($request, $obj->getRequest());
+    }
+
+    /**
+     * Tests the setResponse() method.
+     *
+     * @return void
+     */
+    public function testSetResponse() {
+
+        // Set an Creating API key response mock.
+        $response = new CreatingAPIKeyResponse();
+
+        $obj = new CreatingAPIKeyEvent();
+
+        $obj->setResponse($response);
+        $this->assertSame($response, $obj->getResponse());
+    }
+}
